91d0009f973dccdac753ee0f221eefbc13174cd6
julia <s98435@yandex.ru>
2022-04-26 17:35:46 +0300

    01-CounterButton

--- 01-basics/01-CounterButtonApp/script.js
+++ 01-basics/01-CounterButtonApp/script.js
@@ -1,3 +1,17 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+createApp({
+  data() {
+    return {
+      count: 0,
+    }
+  },
+
+  methods: {
+    handlerClick() {
+      return this.count++;
+    }
+  }
+}).mount('#app')
