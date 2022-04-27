d4dfc7a4cd12cbe7dde3774d61089fc182f003e6
julia <s98435@yandex.ru>
2022-04-27 21:10:51 +0300

    feat: 02-VueCalculator

--- 01-basics/02-VueCalculator/script.js
+++ 01-basics/02-VueCalculator/script.js
@@ -1,3 +1,37 @@
-// import { createApp } from './vendor/vue.esm-browser.js';
+import { createApp } from './vendor/vue.esm-browser.js';
 
 // Создайте Vue приложение
+
+createApp({
+  name: 'Calculator',
+
+  data() {
+    return {
+      first: 0,
+      second: 0,
+      operator: "sum",
+    }
+  },
+
+  computed: {
+    result() {
+      return this[this.operator]()
+    }
+  },
+
+  methods: {
+    sum() {
+      return this.first + this.second
+    },
+    subtract() {
+      return this.first - this.second
+    },
+    multiply() {
+      return this.first * this.second
+    },
+    divide() {
+      return this.first / this.second
+    },
+
+  },
+}).mount('#app')
